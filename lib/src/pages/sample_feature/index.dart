f7a597ec03d3953d3cf152987b659328ee5cd8a9
KitsuneYokai <56147653+KitsuneYokai@users.noreply.github.com>
2023-10-01 18:02:46 +0200

    Testing something
    Testing something out :)

--- lib/src/pages/sample_feature/index.dart
+++ lib/src/pages/sample_feature/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutt_folio/src/pages/auth/login.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 /// Displays a list of SampleItems.
@@ -13,13 +14,19 @@ class IndexView extends StatelessWidget {
   Widget build(BuildContext context) {
     // here all the logic for the view
     return Scaffold(
-        body: Center(
-            child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const LoginView();
-                  }));
-                },
-                child: Text('Test'))));
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return const LoginView();
+            }));
+          },
+          child: Column(children: [
+            Text("Test"),
+            if (kDebugMode) Text("Debug mode"),
+          ]),
+        ),
+      ),
+    );
   }
 }
